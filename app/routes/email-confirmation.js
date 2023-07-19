34f2bc61b3bc2e5d0bf549bc9e82d835c8e9580e
Marc Templeton <marcte@kainos.com>
2023-07-19 09:37:17 +0100

    tidy up

--- app/routes/email-confirmation.js
+++ app/routes/email-confirmation.js
@@ -7,8 +7,7 @@ module.exports = [{
     auth: false
   },
   handler: async (_request, h) => {
-    console.log(_request.payload) // frn comes from here from redirect once auth is required -- initially once click on link, i.e. system/<frn>/statements/1 if not authorised, redirect here with frn as payload, however if auth is killed mid-session then frn payload will be null, therefore redirect out and force to use link again or resend or something
-    _request.payload = { frn: '1234567890' }
+    _request.payload = { frn: '1234567890' } // frn comes from here from redirect once auth is required -- initially once click on link, i.e. system/<frn>/statements/1 if not authorised, redirect here with frn as payload, however if auth is killed mid-session then frn payload will be null, therefore redirect out and force to use link again or resend or something
     return h.view('email-confirmation', { frn: _request.payload.frn, emailAddress: _request.payload?.emailAddress, error: false })
   }
 },
@@ -28,7 +27,6 @@ module.exports = [{
     if (_request.payload.emailAddress !== 'marcte@kainos.com') {
       return h.view('email-confirmation', { frn: _request.payload.frn, emailAddress: _request.payload.emailAddress, error: true })
     }
-    // do cookie thing
     await auth.authenticate(undefined, _request.cookieAuth)
     return h.redirect(`${_request.payload.frn}/statements`)
   }
